0b5b7719d22116fb22927cc1a7985e44f0d50c95
Eric <ericmblomberg@gmail.com>
2015-02-13 18:15:49 -0500

    Updated profile page and scoped status updates

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:jason).profile_name
     assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "only shows the correct user's statuses" do
+  	get :show, id: users(:jason).profile_name
+  	assigns(:statuses).each do |status|
+  		assert_equal users(:jason), status.user
+  	end
   end
 
 end
